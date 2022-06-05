48c130289dba215b8490b0c073e33751a44fbf1d
Jack魏 <1960486253@qq.com>
2022-06-05 09:30:20 +0800

    ParameterDescriptor 1.0翻译版本完成。

--- src/java/beans/ParameterDescriptor.java
+++ src/java/beans/ParameterDescriptor.java
@@ -1,19 +1,16 @@
 /*
  * @(#)ParameterDescriptor.java	1.11 2001/12/12
  *
- * Copyright 2002 Sun Microsystems, Inc. All rights reserved.
- * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * 版权所有 2002 Sun Microsystems, Inc. 保留所有权利。 SUN 专有机密。使用受许可条款的约束。
  */
 
 package java.beans;
 
 /**
- * The ParameterDescriptor class allows bean implementors to provide
- * additional information on each of their parameters, beyond the
- * low level type information provided by the java.lang.reflect.Method
- * class.
- * <p>
- * Currently all our state comes from the FeatureDescriptor base class.
+ * 除了 java.lang.reflect.Method 类提供的低级类型信息之外，
+ * ParameterDescriptor 类允许 bean 实现者提供关于其每个参数的附加信息。
+ *
+ * 目前我们所有的状态都来自 FeatureDescriptor 基类。
  */
 
 public class ParameterDescriptor extends FeatureDescriptor {
