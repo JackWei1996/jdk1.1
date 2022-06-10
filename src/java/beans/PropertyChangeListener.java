58828a7ae8fd90b6f5338cc3de2a146f062fb2d4
Jack魏 <1960486253@qq.com>
2022-06-11 00:40:51 +0800

    翻译1.0完成。

--- src/java/beans/PropertyChangeListener.java
+++ src/java/beans/PropertyChangeListener.java
@@ -1,24 +1,21 @@
 /*
- * @(#)PropertyChangeListener.java	1.11 01/12/12
+ * @(#)PropertyChangeListener.java	1.11 2001/12/12
  *
- * Copyright 2002 Sun Microsystems, Inc. All rights reserved.
- * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * 版权所有 2002 Sun Microsystems, Inc. 保留所有权利。 SUN 专有机密。使用受许可条款的约束。
  */
 
 package java.beans;
 
 /**
- * A "PropertyChange" event gets fired whenever a bean changes a "bound"
- * property.  You can register a PropertyChangeListener with a source
- * bean so as to be notified of any bound property updates.
+ * 每当 bean 更改“绑定”属性时，都会触发“PropertyChange”事件。
+ * 您可以使用源 bean 注册 PropertyChangeListener，以便在任何绑定的属性更新时收到通知。
  */
 
 public interface PropertyChangeListener extends java.util.EventListener {
 
     /**
-     * This method gets called when a bound property is changed.
-     * @param evt A PropertyChangeEvent object describing the event source 
-     *   	and the property that has changed.
+     * 当绑定属性改变时，他的方法被调用。
+     * @param evt 描述事件源和已更改属性的 PropertyChangeEvent 对象。
      */
 
     void propertyChange(PropertyChangeEvent evt);
