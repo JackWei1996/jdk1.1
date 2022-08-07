7df0070c07ae4c2ae9872cef44f568bc7643d3f7
Jack魏 <1960486253@qq.com>
2022-08-07 11:07:31 +0800

    FileNameMap翻译1.0完成。

--- src/java/net/FileNameMap.java
+++ src/java/net/FileNameMap.java
@@ -1,15 +1,13 @@
 /*
- * @(#)FileNameMap.java	1.6 01/12/12
+ * @(#)FileNameMap.java	1.6 2001/12/12
  *
- * Copyright 2002 Sun Microsystems, Inc. All rights reserved.
- * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * 版权所有 2002 Sun Microsystems, Inc. 保留所有权利。 SUN 专有机密。使用受许可条款的约束。
  */
 
 package java.net;
 
 /**
- * A simple interface which provides a mechanism to map between
- * between a file name and a MIME type string.
+ * 一个简单的接口，它提供了一种在文件名和 MIME 类型字符串之间进行映射的机制。
  *
  * @version 	1.6, 12/12/01
  * @author  Steven B. Byrne
