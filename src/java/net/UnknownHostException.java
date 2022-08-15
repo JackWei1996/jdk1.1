b3a8f9fb4306255688679ec104808512048cace1
Jack魏 <1960486253@qq.com>
2022-08-15 22:51:01 +0800

    UnknownHostException 翻译1.0完成。

--- src/java/net/UnknownHostException.java
+++ src/java/net/UnknownHostException.java
@@ -1,8 +1,7 @@
 /*
- * @(#)UnknownHostException.java	1.9 01/12/12
+ * @(#)UnknownHostException.java	1.9 2001/12/12
  *
- * Copyright 2002 Sun Microsystems, Inc. All rights reserved.
- * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * 版权所有 2002 Sun Microsystems, Inc. 保留所有权利。 SUN 专有机密。使用受许可条款的约束。
  */
 
 package java.net;
@@ -10,7 +9,7 @@ package java.net;
 import java.io.IOException;
 
 /**
- * Thrown to indicate that the IP address of a host could not be determined.
+ * 抛出表示无法确定主机的 IP 地址。
  *
  * @author  Jonathan Payne 
  * @version 1.9, 12/12/01
