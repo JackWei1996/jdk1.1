f4c515e29deb1691531b5610bd0427fe934c97a7
Jack魏 <1960486253@qq.com>
2022-08-25 22:10:12 +0800

    NoRouteToHostException 翻译1.0完成。

--- src/java/net/NoRouteToHostException.java
+++ src/java/net/NoRouteToHostException.java
@@ -1,26 +1,20 @@
 /*
- * @(#)NoRouteToHostException.java	1.7 01/12/12
+ * @(#)NoRouteToHostException.java	1.7 2001/12/12
  *
- * Copyright 2002 Sun Microsystems, Inc. All rights reserved.
- * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ * 版权所有 2002 Sun Microsystems, Inc. 保留所有权利。 SUN 专有机密。使用受许可条款的约束。
  */
 
 package java.net;
 
 /**
- * Signals that an error occurred while attempting to connect a
- * socket to a remote address and port.  Typically, the remote
- * host cannot be reached because of an intervening firewall, or
- * if an intermediate router is down.
+ * 表示尝试将套接字连接到远程地址和端口时发生错误的信号。通常，由于中间防火墙或中间路由器已关闭，无法访问远程主机。
  *
  * @since   JDK1.1
  */
 public class NoRouteToHostException extends SocketException {
     /**
-     * Constructs a new NoRouteToHostException with the specified detail 
-     * message as to why the remote host cannot be reached.
-     * A detail message is a String that gives a specific 
-     * description of this error.
+     * 使用指定的详细消息构造一个新的 NoRouteToHostException，以说明无法访问远程主机的原因。
+     * 详细消息是一个字符串，它给出了这个错误的具体描述。
      * @param msg the detail message
      * @since   JDK1.1
      */
